0df7d169cf81802bbc3389e3269fc368d6a62df3
alverodoni <alverodoni22@gmail.com>
2025-01-08 12:02:07 +0700

    Add files via upload

---
+++ data.py
@@ -0,0 +1,9 @@
+class TicketData:
+    def __init__(self):
+        self.tickets = []
+
+    def add_ticket(self, ticket):
+        self.tickets.append(ticket)
+
+    def get_tickets(self):
+        return self.tickets
